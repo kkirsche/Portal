02141bf0d286ea1b63bc2d4c151a36bd32d8bf72
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-05-10 01:07:57 -0400

    Begin testing

--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,7 +1,18 @@
 defmodule PortalTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "should shoot a portal with a color" do
+    case Portal.shoot(:orange) do
+      {:ok, _} -> assert :ok == :ok
+      _        -> assert :ok == :error
+    end
+  end
+
+  test "should transfer data" do
+    {:ok, _} = Portal.shoot(:red)
+    {:ok, _} = Portal.shoot(:blue)
+    portal = Portal.transfer(:red, :blue, [1, 2, 3, 4])
+
+    assert  nil != portal
   end
 end
