7bd8bf73bba724a00a6026e03392f029e42bc1a8
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-05-10 00:09:58 -0400

    Add supervision to processes

--- lib/portal.ex
+++ lib/portal.ex
@@ -9,14 +9,25 @@ defmodule Portal do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3])
+      worker(Portal.Door, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    # We have changed the strategy from :one_for_one to :simple_one_for_one.
+    # Supervisors provide different strategies and :simple_one_for_one is useful
+    # when we want to dynamically create children, often with different arguments.
+    # This is exactly the case for our portal doors, where we want to spawn
+    # multiple doors with different colors.
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  Shoots a new door with the given `color`.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
   end
 
   @doc """
@@ -45,5 +56,24 @@ defmodule Portal do
 
     # Let's return the portal
     portal
+  end
+end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door = inspect(left)
+    right_door = inspect(right)
+
+    left_data = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
   end
 end
